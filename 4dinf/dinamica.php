fd3cfdb5b02612ea930c3f632d5abecccf5205cf
bozzimarcello <bozzimarcello@gmail.com>
2024-06-03 09:20:48 +0200

    aggiunto connessione al db con interrogazione

--- 4dinf/dinamica.php
+++ 4dinf/dinamica.php
@@ -9,6 +9,18 @@
     <?php 
         //questo è codice PHP eseguito lato server
         echo "Today is " . date("Y/m/d h:m:s");
+
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $mysqli = mysqli_connect("localhost", "4dinf", "JZxPv_]*KI1deEQ]", "sakila");
+        
+        $query = "SELECT * FROM actor where first_name LIKE 'B%'";
+
+        $result = mysqli_query($mysqli, $query);
+
+        /* fetch associative array */
+        while ($row = mysqli_fetch_assoc($result)) {
+            printf("<p>%s (%s)</p>", $row["first_name"], $row["last_name"]);
+        }
     ?>
 </body>
 </html>
